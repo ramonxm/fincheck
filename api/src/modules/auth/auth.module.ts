ac18db3f5cc028193ac461c0abc775fda6ef0ba6
Ramon Xavier <ramon1234rxm@gmail.com>
2023-09-27 10:06:42 -0300

    feat: initialize new route endpoint

--- api/src/modules/auth/auth.module.ts
+++ api/src/modules/auth/auth.module.ts
@@ -3,11 +3,11 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { env } from 'src/shared/config/env';
-import { AuthGuard } from './auth.guard';
 
 @Module({
   imports: [
     JwtModule.register({
+      global: true,
       signOptions: { expiresIn: '7d' },
       secret: env.jwtSecret,
     }),
