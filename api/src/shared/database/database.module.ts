1c0c915c65b59a99e7a7552ed6b37d7f31ab560d
Ramon Xavier <ramon1234rxm@gmail.com>
2023-10-10 09:34:34 -0300

    feat: creating resource to categories

--- api/src/shared/database/database.module.ts
+++ api/src/shared/database/database.module.ts
@@ -1,10 +1,11 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { UsersRepository } from './repositories/users.repositories';
+import { CategoriesRepository } from './repositories/categories.repositories';
 
 @Global()
 @Module({
-  providers: [PrismaService, UsersRepository],
+  providers: [PrismaService, UsersRepository, CategoriesRepository],
   exports: [UsersRepository],
 })
 export class DatabaseModule {}
