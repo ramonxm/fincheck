3f145c15d6b1c52f8f46b22723ffaafd87849336
Ramon Xavier <ramon1234rxm@gmail.com>
2024-04-07 20:19:19 +0200

    feat: adding illustration on auth layout

--- fe/src/view/layouts/AuthLayout.tsx
+++ fe/src/view/layouts/AuthLayout.tsx
@@ -1,8 +1,13 @@
 import { Outlet } from 'react-router-dom';
+import illustration from '../../assets/illustration.png';
 
 export const AuthLayout = () => {
   return (
-    <div>
+    <div className="flex w-full h-full">
+      <div className="w-1/2 h-full"></div>
+      <div className="w-1/2 h-full flex justify-center items-center p-8">
+        <img src={illustration} className="object-contain w-full h-full max-w-[656px] max-h-[960px] p-8" />
+      </div>
       <Outlet />
     </div>
   );
