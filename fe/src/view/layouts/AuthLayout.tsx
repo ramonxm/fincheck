4ad05e420b3745ed123c5dfe30b30972a7cb2d90
Ramon Xavier <ramon1234rxm@gmail.com>
2024-04-09 21:20:39 -0300

    feat: adjusting to work on mobile devices

--- fe/src/view/layouts/AuthLayout.tsx
+++ fe/src/view/layouts/AuthLayout.tsx
@@ -8,11 +8,11 @@ export const AuthLayout = () => {
       <div className="w-1/2 h-full flex items-center justify-center flex-col gap-16">
         <Logo className="h-6 text-gray-500" />
 
-        <div className="w-full max-w-md">
+        <div className="w-full max-w-md px-8 lg:px-0">
           <Outlet />
         </div>
       </div>
-      <div className="w-1/2 h-full flex justify-center items-center p-8 relative">
+      <div className="w-1/2 h-full justify-center items-center p-8 relative lg:flex hidden">
         <img
           src={illustration}
           className="object-cover w-full h-full max-w-[656px] max-h-[960px] p-8 select-none rounded-[32px]"
