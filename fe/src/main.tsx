63e10eef2d2d683f8968a44f5d2df00674431e51
Ramon Xavier <ramon1234rxm@gmail.com>
2024-04-29 23:59:51 -0300

    feat: add react-query and hot-toast

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom/client';
 
 import './styles/index.css';
 import { MainRouter } from './app/router/MainRouter';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from 'react-hot-toast';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <MainRouter />
+    <QueryClientProvider client={queryClient}>
+      <Toaster />
+      <MainRouter />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
--- fe/src/view/pages/Register/useRegisterController.ts
+++ fe/src/view/pages/Register/useRegisterController.ts
@@ -1,6 +1,9 @@
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation } from '@tanstack/react-query';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
+import { authService } from '../../../app/services/authService';
+import toast from 'react-hot-toast';
 
 const schema = z.object({
   name: z.string().min(1, 'Nome é obrigatório'),
@@ -17,8 +20,14 @@ export const useRegisterController = () => {
     formState: { errors },
   } = useForm<FormData>({ resolver: zodResolver(schema) });
 
-  const handleSubmit = hookFormHandleSubmit(data => {
-    console.log(data);
+  const mutation = useMutation({ mutationFn: authService.signup });
+
+  const handleSubmit = hookFormHandleSubmit(async data => {
+    try {
+      const { accessToken } = await mutation.mutateAsync(data);
+    } catch (error) {
+      toast.error('Ocorreu um erro ao criar a sua conta!');
+    }
   });
 
   return { handleSubmit, register, errors };
