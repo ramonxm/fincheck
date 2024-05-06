d9efd62fa7c6d2f48bab134c7cca4944ffdb8d76
Ramon Xavier <ramon1234rxm@gmail.com>
2024-05-06 11:02:37 -0300

    feat: call /me and add accesstoken in endpoints

--- fe/src/app/contexts/AuthContext.tsx
+++ fe/src/app/contexts/AuthContext.tsx
@@ -1,5 +1,8 @@
-import { createContext, PropsWithChildren, useCallback, useState } from 'react';
+import { createContext, PropsWithChildren, useCallback, useEffect, useState } from 'react';
 import { localStorageKeys } from '../config/localStorageKeys';
+import { useQuery } from '@tanstack/react-query';
+import { usersService } from '../services/usersService';
+import { httpClient } from '../services/httpClient';
 
 interface AuthContextValue {
   signedIn: boolean;
@@ -16,11 +19,29 @@ export const AuthProvider = ({ children }: PropsWithChildren) => {
     return Boolean(storedAccessToken);
   });
 
-  const signin = useCallback((accessToken: string) => {
-    localStorage.setItem(localStorageKeys.ACCESS_TOKEN, accessToken);
-
-    setSignedIn(true);
+  const setAccessToken = useCallback((accessToken: string) => {
+    httpClient.defaults.headers.Authorization = `Bearer ${accessToken}`;
   }, []);
+
+  useEffect(() => {
+    const storedAccessToken = localStorage.getItem(localStorageKeys.ACCESS_TOKEN);
+
+    if (storedAccessToken) {
+      setAccessToken(storedAccessToken);
+    }
+  }, [setAccessToken]);
+
+  useQuery({ queryFn: usersService.me, queryKey: ['loggedUser'], enabled: signedIn });
+
+  const signin = useCallback(
+    (accessToken: string) => {
+      localStorage.setItem(localStorageKeys.ACCESS_TOKEN, accessToken);
+      setAccessToken(accessToken);
+
+      setSignedIn(true);
+    },
+    [setAccessToken],
+  );
 
   const signout = useCallback(() => {
     localStorage.removeItem(localStorageKeys.ACCESS_TOKEN);
--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -7,7 +7,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
 import { AuthProvider } from './app/contexts/AuthContext';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({ defaultOptions: { queries: { retry: false, refetchOnWindowFocus: false } } });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
